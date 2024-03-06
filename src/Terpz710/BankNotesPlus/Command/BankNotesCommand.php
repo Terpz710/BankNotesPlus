769074b864cc5b9d9e09285b7cf4de4ae6e499ec
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2024-03-06 14:02:13 -0800

    Fix: Lots of fixes

--- src/Terpz710/BankNotesPlus/Command/BankNotesCommand.php
+++ src/Terpz710/BankNotesPlus/Command/BankNotesCommand.php
@@ -1,24 +1,27 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Terpz710\BankNotesPlus\Command;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 use pocketmine\utils\TextFormat as TF;
+
 use Terpz710\BankNotesPlus\BankNotesPlus;
-use davidglitch04\libEco\libEco;
+use Terpz710\BankNotesPlus\Economy\EconomyManager;
 
 class BankNotesCommand extends Command {
 
     private $plugin;
-    private $libEco;
+    private $economyManager;
 
-    public function __construct(BankNotesPlus $plugin, libEco $libEco) {
+    public function __construct(BankNotesPlus $plugin, EconomyManager $economyManager) {
         parent::__construct("banknote", "Convert in-game money into bank notes", "/banknote {amount}");
         $this->setPermission("banknotesplus.cmd");
         $this->plugin = $plugin;
-        $this->libEco = $libEco;
+        $this->economyManager = $economyManager;
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args) {
@@ -28,20 +31,14 @@ class BankNotesCommand extends Command {
 
         if ($sender instanceof Player) {
             if (count($args) === 1 && is_numeric($args[0]) && $args[0] > 0) {
-                $amount = (float)$args[0];
+                $amount = (int)$args[0];
 
-                $this->libEco->myMoney($sender, function($balance) use ($sender, $amount) {
-                    if ($balance >= $amount) {
-                        $this->libEco->reduceMoney($sender, $amount, function($success) use ($sender, $amount) {
-                            if ($success) {
-                                $this->plugin->convertToBankNote($sender, $amount);
-                                $sender->sendMessage(TF::GREEN . "You have converted §f$" . $amount . " §ainto a bank note.");
-                            } else {
-                                $sender->sendMessage(TF::RED . "Failed to reduce money. Please try again.");
-                            }
-                        });
+                $this->economyManager->reduceMoney($sender, $amount, function($success) use ($sender, $amount) {
+                    if ($success) {
+                        $this->plugin->convertToBankNote($sender, $amount);
+                        $sender->sendMessage(TF::GREEN . "You have converted §f$" . $amount . " §ainto a bank note.");
                     } else {
-                        $sender->sendMessage(TF::RED . "You don't have enough money to convert into a bank note.");
+                        $sender->sendMessage(TF::RED . "Failed to reduce money. Please try again.");
                     }
                 });
             } else {
