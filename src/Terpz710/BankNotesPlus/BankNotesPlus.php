095ae84a581a03226a86be82b3f25bc5852325da
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2024-03-06 14:01:22 -0800

    Fix: Lots of fixes

--- src/Terpz710/BankNotesPlus/BankNotesPlus.php
+++ src/Terpz710/BankNotesPlus/BankNotesPlus.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Terpz710\BankNotesPlus;
 
 use pocketmine\item\enchantment\EnchantmentInstance;
@@ -10,16 +12,18 @@ use pocketmine\item\VanillaItems;
 use pocketmine\plugin\PluginBase;
 use pocketmine\player\Player;
 use pocketmine\utils\TextFormat as TF;
+
 use Terpz710\BankNotesPlus\Command\BankNotesCommand;
-use davidglitch04\libEco\libEco;
+use Terpz710\BankNotesPlus\Economy\EconomyManager;
 
 class BankNotesPlus extends PluginBase implements Listener {
 
-    public function onEnable(): void {
-        $libEco = new libEco();
+    private $economyManager;
 
+    public function onEnable(): void {
+        $this->economyManager = new EconomyManager($this);
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->getServer()->getCommandMap()->register("banknote", new BankNotesCommand($this, $libEco));
+        $this->getServer()->getCommandMap()->register("banknote", new BankNotesCommand($this, $this->economyManager));
     }
 
     public function onPlayerInteract(PlayerInteractEvent $event): void {
@@ -27,32 +31,29 @@ class BankNotesPlus extends PluginBase implements Listener {
         $item = $event->getItem();
 
         if ($item->getNamedTag()->getTag("Amount") !== null) {
-            $amount = $item->getNamedTag()->getFloat("Amount");
-
+            $amount = $item->getNamedTag()->getInt("Amount");
             $item->setCount($item->getCount() - 1);
             $player->getInventory()->setItemInHand($item);
-
-            libEco::addMoney($player, $amount);
-            $player->sendMessage(TF::GREEN . "You have claimed §f$" . $amount . " §afrom the bank note.");
+            $this->economyManager->addMoney($player, $amount, function($success) use ($player, $amount) {
+                if ($success) {
+                    $player->sendMessage(TF::GREEN . "You have claimed §f$" . $amount . " §afrom the bank note.");
+                } else {
+                    $player->sendMessage(TF::RED . "Failed to add money. Please try again.");
+                }
+            });
         }
     }
 
-    public function hasEnoughMoney(Player $player, float $amount): bool {
-        return true;
-    }
-
-    public function convertToBankNote(Player $player, float $amount): void {
+    public function convertToBankNote(Player $player, int $amount): void {
         $bankNote = VanillaItems::PAPER();
         $bankNote->setCustomName(TF::GOLD . "$" . $amount . " Bank Note");
         $bankNote->setLore([
             "Value: $" . $amount,
             "Right-click to redeem"
         ]);
-        $bankNote->getNamedTag()->setFloat("Amount", $amount);
-
+        $bankNote->getNamedTag()->setInt("Amount", $amount);
         $enchantment = new EnchantmentInstance(VanillaEnchantments::FORTUNE(), 3);
         $bankNote->addEnchantment($enchantment);
-
         $player->getInventory()->addItem($bankNote);
     }
 }
