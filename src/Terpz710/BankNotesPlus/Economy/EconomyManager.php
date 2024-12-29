28555cc5ced22ccb30d967a8be12e7ed09eaee7b
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2024-12-29 04:57:03 -0800

    Added missing function(Report #2)

--- src/Terpz710/BankNotesPlus/Economy/EconomyManager.php
+++ src/Terpz710/BankNotesPlus/Economy/EconomyManager.php
@@ -2,86 +2,107 @@
 
 declare(strict_types=1);
 
-namespace Terpz710\BankNotesPlus\Economy;
+namespace wavycraft\antivoid\economy;
 
 use Closure;
+
 use pocketmine\player\Player;
 
+use pocketmine\plugin\Plugin;
+
 use onebone\economyapi\EconomyAPI;
+
 use cooldogedev\BedrockEconomy\api\type\ClosureAPI;
-use cooldogedev\BedrockEconomy\BedrockEconomy;
 use cooldogedev\BedrockEconomy\api\BedrockEconomyAPI;
 use cooldogedev\BedrockEconomy\currency\Currency;
 use cooldogedev\BedrockEconomy\database\cache\GlobalCache;
+use cooldogedev\BedrockEconomy\BedrockEconomy;
 
 use Terpz710\BankNotesPlus\BankNotesPlus;
 
-class EconomyManager {
+final class EconomyManager {
 
-    private $eco;
+    private ?Plugin $eco;
     private ?ClosureAPI $api;
-    private Currency $currency;
+    private ?Currency $currency;
     private BankNotesPlus $plugin;
 
     public function __construct(BankNotesPlus $plugin) {
         $this->plugin = $plugin;
-        $manager = $this->plugin->getServer()->getPluginManager();
+        $manager = $plugin->getServer()->getPluginManager();
         $this->eco = $manager->getPlugin("EconomyAPI") ?? $manager->getPlugin("BedrockEconomy") ?? null;
 
         if ($this->eco instanceof BedrockEconomy) {
             $this->api = BedrockEconomyAPI::CLOSURE();
             $this->currency = BedrockEconomy::getInstance()->getCurrency();
+        } else {
+            $this->api = null;
+            $this->currency = null;
         }
 
-        if ($this->eco === null) {
-            $this->plugin->getLogger()->warning("ERROR: No compatible economy plugin found. Please install EconomyAPI or BedrockEconomy");
-        }
+        unset($manager);
     }
 
     public function getMoney(Player $player, Closure $callback): void {
-        if ($this->eco === null) {
+        if ($this->eco instanceof EconomyAPI) {
+            $money = $this->eco->myMoney($player->getName());
+            assert(is_float($money));
+            $callback($money);
+        } elseif ($this->eco instanceof BedrockEconomy) {
+            $entry = GlobalCache::ONLINE()->get($player->getName());
+            $callback($entry ? (float)"{$entry->amount}.{$entry->decimals}" : (float)"{$this->currency->defaultAmount}.{$this->currency->defaultDecimals}");
+        } else {
             $callback(0.0);
+        }
+    }
+
+    public function reduceMoney(Player $player, int $amount, Closure $callback): void {
+        if ($this->eco === null) {
+            $this->plugin->getLogger()->warning("You don't have an Economy plugin");
             return;
         }
 
-        switch ($this->eco->getName()) {
-            case "EconomyAPI":
-                $money = $this->eco->myMoney($player->getName());
-                $callback(is_numeric($money) ? (float)$money : 0.0);
-                break;
-            case "BedrockEconomy":
-                $entry = GlobalCache::ONLINE()->get($player->getName());
-                $callback($entry ? (float)"{$entry->amount}.{$entry->decimals}" : (float)"{$this->currency->defaultAmount}.{$this->currency->defaultDecimals}");
-                break;
-            default:
-                $callback(0.0);
+        if ($this->eco instanceof EconomyAPI) {
+            $callback($this->eco->reduceMoney($player->getName(), $amount) === EconomyAPI::RET_SUCCESS);
+        } elseif ($this->eco instanceof BedrockEconomy) {
+            $decimals = (int)(explode('.', strval($amount))[1] ?? 0);
+            $this->api->subtract(
+                $player->getXuid(),
+                $player->getName(),
+                (int)$amount,
+                $decimals,
+                function() use ($callback) {
+                    $callback(true);
+                },
+                function() use ($callback) {
+                    $callback(false);
+                }
+            );
         }
     }
 
     public function addMoney(Player $player, int $amount, Closure $callback): void {
         if ($this->eco === null) {
             $this->plugin->getLogger()->warning("You don't have an Economy plugin");
-            $callback(false);
             return;
         }
 
-        switch ($this->eco->getName()) {
-            case "EconomyAPI":
-                $callback($this->eco->addMoney($player->getName(), $amount) === EconomyAPI::RET_SUCCESS);
-                break;
-            case "BedrockEconomy":
-                $decimals = (int)(explode('.', strval($amount))[1] ?? 0);
-                $this->api->add(
-                    $player->getXuid(),
-                    $player->getName(),
-                    (int)$amount,
-                    $decimals,
-                    fn() => $callback(true),
-                    fn() => $callback(false)
-                );
-                break;
-            default:
-                $callback(false);
+        if ($this->eco instanceof EconomyAPI) {
+            $callback($this->eco->addMoney($player->getName(), $amount) === EconomyAPI::RET_SUCCESS);
+        } elseif ($this->eco instanceof BedrockEconomy) {
+            $decimals = (int)(explode('.', strval($amount))[1] ?? 0);
+            $this->api->add(
+                $player->getXuid(),
+                $player->getName(),
+                (int)$amount,
+                $decimals,
+                function() use ($callback) {
+                    $callback(true);
+                },
+                function() use ($callback) {
+                    $callback(false);
+                }
+            );
         }
     }
 }
