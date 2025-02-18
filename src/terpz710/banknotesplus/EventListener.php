f5565df586ff033346ce14fc13fbc6e38b46496d
WavyCraftNetwork <142659461+Terpz710@users.noreply.github.com>
2025-02-18 03:44:58 -0800

    Create EventListener.php

---
+++ src/terpz710/banknotesplus/EventListener.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace terpz710\banknotesplus;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+
+class EventListener implements Listener {
+
+}
