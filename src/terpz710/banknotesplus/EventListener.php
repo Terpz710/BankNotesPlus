508a68913a1b6fc74108588a8937d4e05e0e4a82
WavyCraftNetwork <142659461+Terpz710@users.noreply.github.com>
2025-02-18 03:55:43 -0800

    Update EventListener.php

--- src/terpz710/banknotesplus/EventListener.php
+++ src/terpz710/banknotesplus/EventListener.php
@@ -7,6 +7,32 @@ namespace terpz710\banknotesplus;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
 
+use terpz710\banknotesplus\economy\EconomyManager;
+
 class EventListener implements Listener {
 
+    public function onPlayerInteract(PlayerInteractEvent $event) : void{
+        $player = $event->getPlayer();
+        $item = $event->getItem();
+        $action = $event->getAction();
+
+        if ($action === PlayerInteractEvent::LEFT_CLICK_BLOCK || $action === PlayerInteractEvent::RIGHT_CLICK_BLOCK) {
+            if ($item->getNamedTag()->getTag("Amount")) {
+                $amount = $item->getNamedTag()->getInt("Amount");
+                $item->setCount($item->pop());
+                $player->getInventory()->setItemInHand($item);
+                EconomyManager::getInstance()->addMoney($player, $amount, function($success) use ($player, $amount, $event) {
+                    if ($success) {
+                        $message = BankNotesPlus::getInstance()->getConfig()->get("claim_message");
+                        $message = str_replace("{amount}", (string)$amount, $message);
+                        $player->sendMessage($message);
+                    } else {
+                        $message = BankNotesPlus::getInstance()->getConfig()->get("failure_message");
+                        $player->sendMessage($message);
+                        $event->cancel();
+                    }
+                });
+            }
+        }
+    }
 }
